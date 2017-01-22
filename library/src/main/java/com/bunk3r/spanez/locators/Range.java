e073f41d6f9f0cbe5a0e04fada603b4930984373
unknown <joragu@YomBunkerLap.redmond.corp.microsoft.com>
2017-01-22 00:28:42 -0800

    - Refactor of the code to remove code smells

--- library/src/main/java/com/bunk3r/spanez/locators/Range.java
+++ library/src/main/java/com/bunk3r/spanez/locators/Range.java
@@ -17,28 +17,28 @@ import java.util.List;
 
 @SuppressWarnings("WeakerAccess")
 public final class Range implements Locator {
-    private final TargetRange range;
+    private final TargetRange targetRange;
     private final List<TargetRange> ranges = new ArrayList<>(1);
     private final List<TargetRange> empty = Collections.emptyList();
 
     public static Range from(@IntRange(from = 0) int start, @IntRange(from = 0) int end) {
         if (end < start) {
-            throw new IllegalArgumentException("The end of the range shouldn't be before the start");
+            throw new IllegalArgumentException("The end of the targetRange shouldn't be before the start");
         }
 
         return new Range(start, end);
     }
 
     private Range(int start, int end) {
-        range = TargetRange.from(start, end);
-        ranges.add(range);
+        targetRange = TargetRange.from(start, end);
+        ranges.add(targetRange);
     }
 
     @NonNull
     @Override
     public List<TargetRange> locate(@NonNull String content) {
         final int contentLength = content.length();
-        return (range.getStart() < contentLength && range.getEnd() < contentLength)
+        return (targetRange.getStart() < contentLength && targetRange.getEnd() < contentLength)
                 ? ranges
                 : empty;
     }
